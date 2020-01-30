f38ccd9c55380409d87b5addcdcacb61c9cd90f3
Katie Farrell <kfarrell064@gmail.com>
2020-01-30 18:05:27 -0500

    fixed image link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,31 +13,35 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
+  get '/fbr.erb' do
+    erb :fbr
+  end
+
   get '/construction.erb' do
     erb :construction
   end
-  
+
   get '/bananasforscale.erb' do
     erb :bananasforscale
   end
-  
+
   get '/meganproofer.erb' do
     erb :meganproofer
   end
-  
+
   get '/dabjarcalculator.erb' do
     erb :dabjarcalculator
   end
-  
+
   get '/cdd.erb' do
     erb :cdd
   end
- 
+
  get '/javaprojects.erb' do
     erb :javaprojects
   end
-  
+
   post '/uselesscalculating' do
     @unitval = params["unitVal"]
     @unittype = params["unitType"]
@@ -45,13 +49,13 @@ class ApplicationController < Sinatra::Base
     @uselessanswer = howMany(@unitval)
     erb :uselessanswer
   end
-  
+
   post '/meganproofd' do
     @string = params["string"]
     @proofdanswer = meganProof(@string)
     erb :meganproofd
   end
-  
+
   post '/totalfine' do
   @dabfrequency = params["dabfrequency"]
   @dabduration = params["dabduration"]
